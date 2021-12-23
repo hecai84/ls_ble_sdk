376c4b803fbeee317839d96b552fdba7a91442c6
mingzhou-ls <mingzhou@linkedsemi.com>
2021-12-23 15:10:58 +0800

    ssi_test: Update ssi test demo.

--- dev/examples/peripheral/ssi/ssi_dma/main.c
+++ dev/examples/peripheral/ssi/ssi_dma/main.c
@@ -27,6 +27,8 @@
 #include "lsssi.h"
 #include "lsdmac.h"
 
+#define SSI_TEST_BUF_SIZE 4
+#define SSI_TEST_LED_IO PA01
 /* Private function prototypes -----------------------------------------------*/
 static void Error_Handler(void);
 
@@ -35,77 +37,83 @@ static void Error_Handler(void);
 SSI_HandleTypeDef SsiHandle;
 
 /* SSI TxRx flag */
-static uint8_t ssi_txrx_flag = 0;
+volatile static uint8_t ssi_txrx_flag = 1;
 
-DMA_RAM_ATTR uint8_t ssi_dma_tx_buf[4];
-DMA_RAM_ATTR uint8_t ssi_dma_rx_buf[4];
+DMA_RAM_ATTR uint8_t ssi_dma_tx_buf[SSI_TEST_BUF_SIZE];
+DMA_RAM_ATTR uint8_t ssi_dma_rx_buf[SSI_TEST_BUF_SIZE];
 
 DEF_DMA_CONTROLLER(dmac1_SSI_inst,DMAC1);
 
 void HAL_SSI_TxRxDMACpltCallback(SSI_HandleTypeDef *hssi)
 {
-	LOG_I("RX CMPLT: 0x%x, 0x%x, 0x%x, 0x%x", ssi_dma_rx_buf[0],ssi_dma_rx_buf[1],ssi_dma_rx_buf[2],ssi_dma_rx_buf[3]);
-	ssi_txrx_flag = 1;
-	io_toggle_pin(PA01);
+    LOG_I("DMA TXRX CMPLT: 0x%x, 0x%x, 0x%x, 0x%x", ssi_dma_rx_buf[0],ssi_dma_rx_buf[1],ssi_dma_rx_buf[2],ssi_dma_rx_buf[3]);
+    if (0 == memcmp(&ssi_dma_tx_buf[0], &ssi_dma_rx_buf[0], SSI_TEST_BUF_SIZE))
+    {
+        ssi_txrx_flag = 1;
+        io_toggle_pin(SSI_TEST_LED_IO);
+    }
+    else
+    {
+        LOG_I("Data unmatched!!!");
+        LS_ASSERT(0);
+    }
 }
 
 static void ssi_test_init(void)
 {
-    ssi_clk_io_init(PB09);		/* CLK-------------PB09 */	
-	ssi_nss0_io_init(PB08);	/* SSN-------------PB08 */	
-	ssi_dq0_io_init(PA07);		/* MOSI------------PA07 */	
-	ssi_dq1_io_init(PA00);		/* MISO------------PA00 */	
-	
-	SsiHandle.REG = LSSSI;
-	SsiHandle.Init.clk_div = 64;
-	SsiHandle.Init.rxsample_dly = 0;
-	SsiHandle.Init.ctrl.cph = SCLK_Toggle_In_Middle;
-	SsiHandle.Init.ctrl.cpol = Inactive_Low;
-	SsiHandle.Init.ctrl.data_frame_size = DFS_32_8_bits;
+    ssi_clk_io_init(PB09);        /* CLK-------------PB09 */    
+    ssi_nss0_io_init(PB08);    /* SSN-------------PB08 */    
+    ssi_dq0_io_init(PA07);        /* MOSI------------PA07 */    
+    ssi_dq1_io_init(PA00);        /* MISO------------PA00 */    
+    
+    SsiHandle.REG = LSSSI;
+    SsiHandle.Init.clk_div = 64;
+    SsiHandle.Init.rxsample_dly = 0;
+    SsiHandle.Init.ctrl.cph = SCLK_Toggle_In_Middle;
+    SsiHandle.Init.ctrl.cpol = Inactive_Low;
+    SsiHandle.Init.ctrl.data_frame_size = DFS_32_8_bits;
 
     DMA_CONTROLLER_INIT(dmac1_SSI_inst);
-	SsiHandle.DMAC_Instance = &dmac1_SSI_inst;
-	SsiHandle.Tx_Env.DMA.DMA_Channel = 0;
-	SsiHandle.Rx_Env.DMA.DMA_Channel = 1;
+    SsiHandle.DMAC_Instance = &dmac1_SSI_inst;
+    SsiHandle.Tx_Env.DMA.DMA_Channel = 0;
+    SsiHandle.Rx_Env.DMA.DMA_Channel = 1;
 
-	if(HAL_SSI_Init(&SsiHandle) != HAL_OK)
+    if(HAL_SSI_Init(&SsiHandle) != HAL_OK)
     {
         /* Initialization Error */
         Error_Handler();
     }
 }
 
+static void ssi_test_data_vary(void)
+{
+    for (uint16_t i = 0; i < SSI_TEST_BUF_SIZE; i++)
+    {
+        ssi_dma_tx_buf[i] += rand() % 256;
+    }
+}
+
 int main(void)
 {
-	sys_init_none();
+    sys_init_none();
     ssi_test_init();
 
-    ssi_dma_tx_buf[0] = 0x9F;
-	ssi_dma_tx_buf[1] = 0xAA;
-	ssi_dma_tx_buf[2] = 0x55;
-	ssi_dma_tx_buf[3] = 0xAA;
+    io_cfg_output(SSI_TEST_LED_IO);
 
-      /* While the SPI in TransmitReceive process, user can transmit data through 
-     "aTxBuffer" buffer & receive data through "aRxBuffer" */
-    if(HAL_SSI_TransmitReceive_DMA(&SsiHandle, ssi_dma_tx_buf,ssi_dma_rx_buf, 4)!= HAL_OK)
+    while(1)
     {
-        /* Transfer error in transmission process */
-        Error_Handler();
-    }
-
-	while(1)
-	{
-		if(ssi_txrx_flag == 1)
-		{
-			if(HAL_SSI_TransmitReceive_DMA(&SsiHandle, ssi_dma_tx_buf,ssi_dma_rx_buf, 4)!= HAL_OK)
+        if(ssi_txrx_flag == 1)
+        {
+            DELAY_US(50000);
+            ssi_txrx_flag = 0;
+            ssi_test_data_vary();
+            if(HAL_SSI_TransmitReceive_DMA(&SsiHandle, ssi_dma_tx_buf,ssi_dma_rx_buf, SSI_TEST_BUF_SIZE)!= HAL_OK)
             {
                 /* Transfer error in transmission process */
                 Error_Handler();
             }
-			ssi_txrx_flag = 0;
-		}
-		DELAY_US(10000);
-	}
+        }
+    }
 }
 
 /**
--- dev/examples/peripheral/ssi/ssi_it/main.c
+++ dev/examples/peripheral/ssi/ssi_it/main.c
@@ -27,6 +27,8 @@
 #include "lsssi.h"
 #include "lsdmac.h"
 
+#define SSI_TEST_BUF_SIZE 4
+#define SSI_TEST_LED_IO PA01
 /* Private function prototypes -----------------------------------------------*/
 static void Error_Handler(void);
 
@@ -35,68 +37,79 @@ static void Error_Handler(void);
 SSI_HandleTypeDef SsiHandle;
 
 /* Buffer used for transmission */
-static uint8_t ssi_tx_buf[4] = {0x9F,0xAA,0x55,0XAA};
+static uint8_t ssi_tx_buf[SSI_TEST_BUF_SIZE] = {0x9F,0xAA,0x55,0XAA};
 
 /* Buffer used for reception */
-static uint8_t ssi_rx_buf[4] = {0};
+static uint8_t ssi_rx_buf[SSI_TEST_BUF_SIZE] = {0};
 
 /* SSI TX flag */
-static uint8_t ssi_txrx_flag = 0;
-
+volatile static uint8_t ssi_txrx_flag = 1;
 
 void HAL_SSI_TxRxCpltCallback(SSI_HandleTypeDef *hssi)
 {
-	LOG_I("TXRX CMPLT: 0x%x, 0x%x, 0x%x, 0x%x", ssi_rx_buf[0],ssi_rx_buf[1],ssi_rx_buf[2],ssi_rx_buf[3]);
-	ssi_txrx_flag = 1;
+    LOG_I("TXRX CMPLT: 0x%x, 0x%x, 0x%x, 0x%x", ssi_rx_buf[0],ssi_rx_buf[1],ssi_rx_buf[2],ssi_rx_buf[3]);
+    if (0 == memcmp(&ssi_tx_buf[0], &ssi_rx_buf[0], SSI_TEST_BUF_SIZE))
+    {
+        ssi_txrx_flag = 1;
+        io_toggle_pin(SSI_TEST_LED_IO);
+    }
+    else
+    {
+        LOG_I("Data unmatched!!!");
+        LS_ASSERT(0);
+    }
 }
 
 static void ssi_test_init(void)
 {
-    ssi_clk_io_init(PB09);		/* CLK-------------PB09 */	
-	ssi_nss0_io_init(PB08);	/* SSN-------------PB08 */	
-	ssi_dq0_io_init(PA07);		/* MOSI------------PA07 */	
-	ssi_dq1_io_init(PA00);		/* MISO------------PA00 */	
-	
-	SsiHandle.REG = LSSSI;
-	SsiHandle.Init.clk_div = 128;
-	SsiHandle.Init.rxsample_dly = 0;
-	SsiHandle.Init.ctrl.cph = SCLK_Toggle_In_Middle;
-	SsiHandle.Init.ctrl.cpol = Inactive_Low;
-	SsiHandle.Init.ctrl.data_frame_size = DFS_32_8_bits;
+    ssi_clk_io_init(PB09);        /* CLK-------------PB09 */    
+    ssi_nss0_io_init(PB08);    /* SSN-------------PB08 */    
+    ssi_dq0_io_init(PA07);        /* MOSI------------PA07 */    
+    ssi_dq1_io_init(PA00);        /* MISO------------PA00 */    
+    
+    SsiHandle.REG = LSSSI;
+    SsiHandle.Init.clk_div = 128;
+    SsiHandle.Init.rxsample_dly = 0;
+    SsiHandle.Init.ctrl.cph = SCLK_Toggle_In_Middle;
+    SsiHandle.Init.ctrl.cpol = Inactive_Low;
+    SsiHandle.Init.ctrl.data_frame_size = DFS_32_8_bits;
 
-	if(HAL_SSI_Init(&SsiHandle) != HAL_OK)
+    if(HAL_SSI_Init(&SsiHandle) != HAL_OK)
     {
         /* Initialization Error */
         Error_Handler();
     }
 }
 
+static void ssi_test_data_vary(void)
+{
+    for (uint16_t i = 0; i < SSI_TEST_BUF_SIZE; i++)
+    {
+        ssi_tx_buf[i] += rand() % 256;
+    }
+}
+
 int main(void)
 {
-	sys_init_none();
+    sys_init_none();
     ssi_test_init();
 
-      /* While the SPI in TransmitReceive process, user can transmit data through 
-     "aTxBuffer" buffer & receive data through "aRxBuffer" */
-    if(HAL_SSI_TransmitReceive_IT(&SsiHandle, (uint8_t*)ssi_tx_buf, (uint8_t *)ssi_rx_buf, 4) != HAL_OK)
+    io_cfg_output(SSI_TEST_LED_IO);
+    while(1)
     {
-        /* Transfer error in transmission process */
-        Error_Handler();
-    }
-
-	while(1)
-	{
-		if(ssi_txrx_flag == 1)
-		{
-			if(HAL_SSI_TransmitReceive_IT(&SsiHandle, (uint8_t*)ssi_tx_buf, (uint8_t *)ssi_rx_buf, 4) != HAL_OK)
+        if(ssi_txrx_flag == 1)
+        {
+            DELAY_US(50000);
+            ssi_txrx_flag = 0;
+            ssi_test_data_vary();
+            if(HAL_SSI_TransmitReceive_IT(&SsiHandle, (uint8_t*)ssi_tx_buf, (uint8_t *)ssi_rx_buf, SSI_TEST_BUF_SIZE) != HAL_OK)
             {
                 /* Transfer error in transmission process */
                 Error_Handler();
             }
-			ssi_txrx_flag = 0;
-		}
-		DELAY_US(10000);
-	}
+        }
+        
+    }
 }
 
 /**
